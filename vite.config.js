a4a4be99e41624c822b4a6d879cbea63a2e663c1
ReaganJohnston <hyerx01@gmail.com>
2021-08-12 21:44:17 +1000

    updated chunk limit for vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import build from 'vite'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  build: {
+    chunkSizeWarningLimit: 10000000
+  }
 })
